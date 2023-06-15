7ad585dcebf3a04950ee4e84037794a77e3eb23f
Kayla S <kaylaesmith1@gmail.com>
2023-06-15 10:25:06 +0000

    Create IsOwner permission, login/logout views

---
+++ activities_backend_api/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+
+# Allows you to only edit your profile if you're logged in.
+class IsOwnerOrReadOnly(permissions.BasePermission):
+  def has_object_permission(self, request, view, obj):
+    if request.method in permissions.SAFE_METHODS:
+      return True
+    return obj.owner == request.user
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,15 +4,15 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    # is_owner = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
     # following_id = serializers.SerializerMethodField()
     # posts_count = serializers.ReadOnlyField()
     # followers_count = serializers.ReadOnlyField()
     # following_count = serializers.ReadOnlyField()
 
-    # def get_is_owner(self, obj):
-    #     request = self.context['request']
-    #     return request.user == obj.owner
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     # def get_following_id(self, obj):
     #     user = self.context['request'].user
@@ -27,7 +27,7 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image'
+            'content', 'image', 'is_owner'
         ]
 
-        # , 'is_owner', 'following_id', 'posts_count', 'followers_count', 'following_count'
+        #  'following_id', 'posts_count', 'followers_count', 'following_count'
