3d8879359570596c68d80edc7e3751d3e84d6b83
Kayla S <kaylaesmith1@gmail.com>
2023-07-07 15:47:44 +0000

    Change allowed hosts back to local

--- activities_backend_api/settings.py
+++ activities_backend_api/settings.py
@@ -65,10 +65,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEV' in os.environ
-DEBUG = False
+DEBUG = True
 
-# ALLOWED_HOSTS = ['localhost', '8000-kaylaesmith-activitiesb-lhjnqryo3hg.ws-eu101.gitpod.io']
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
+ALLOWED_HOSTS = ['localhost', '8000-kaylaesmith-activitiesb-lhjnqryo3hg.ws-eu101.gitpod.io']
+# ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
 
 # Application definition
 
